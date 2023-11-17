865ad090bcbe4d0f124014e2d345c0ddf10b1317
Andras Varro <av.reg@yahoo.com>
2023-11-16 22:51:25 -0500

    Remove unnecessary comments.

--- Page/script.js
+++ Page/script.js
@@ -5,55 +5,7 @@ const ctx = canvas.getContext("2d");
 const canvasColor="rgb(0, 0, 0)"
 ctx.fillStyle = canvasColor;
 ctx.fillRect(0, 0, width, height);
-
-/*
-ctx.fillStyle = "rgb(255, 0, 0)";
-ctx.fillRect(50, 50, 100, 150);
-
-ctx.fillStyle = "rgb(0, 255, 0)";
-ctx.fillRect(75, 75, 100, 100);
-ctx.fillStyle = "rgba(255, 0, 255, 0.75)";
-ctx.fillRect(25, 100, 175, 50);
-
-ctx.strokeStyle = "rgb(255, 255, 255)";
-ctx.lineWidth = 5;
-ctx.strokeRect(25, 25, 175, 200);
-
-ctx.fillStyle = "rgb(255, 0, 0)";
-ctx.beginPath();
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-const triHeight = 50 * Math.tan(degToRad(60));
-ctx.lineTo(100, 50 + triHeight);
-ctx.lineTo(50, 50);
-ctx.fill();
-
-ctx.fillStyle = "rgb(0, 0, 255)";
-ctx.beginPath();
-ctx.arc(150, 106, 50, degToRad(0), degToRad(360), false);
-ctx.fill();
-
-ctx.fillStyle = "yellow";
-ctx.beginPath();
-ctx.arc(200, 106, 50, degToRad(-45), degToRad(45), true);
-ctx.lineTo(200, 106);
-ctx.fill();
-
-ctx.strokeStyle = "white";
-ctx.lineWidth = 1;
-ctx.font = "36px arial";
-ctx.strokeText("Canvas text", 50, 50);
-
-ctx.fillStyle = "red";
-ctx.font = "48px georgia";
-ctx.fillText("Canvas text", 50, 150);
-canvas.setAttribute("aria-label", "Canvas text");
-
-const image = new Image();
-image.src = "Retropie2s.png";
-image.addEventListener("load", () => ctx.drawImage(image, 20, 20));
-*/
-
+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
 function degToRad(degrees) {
   return (degrees * Math.PI) / 180;
 }
@@ -62,35 +14,9 @@ function rand(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-/*ctx.translate(width / 2, height / 2);
-
-let length = 250;
-let moveOffset = 20;
-
-for (let i = 0; i < length; i++) {
-	ctx.fillStyle = `rgba(${255 - length},0,${255 - length},0.9)`;
-	ctx.beginPath();
-	ctx.moveTo(moveOffset, moveOffset);
-	ctx.lineTo(moveOffset + length, moveOffset);
-	const triHeight = (length / 2) * Math.tan(degToRad(60));
-	ctx.lineTo(moveOffset + length / 2, moveOffset + triHeight);
-	ctx.lineTo(moveOffset, moveOffset);
-	ctx.fill();
-
-	length--;
-	moveOffset += 0.7;
-	ctx.rotate(degToRad(5));
-}*/
-
 const numberOfBits=16;
 const numberOfRegisters=8;
-const onColor1="rgba(255, 0, 0, 0.25)";
-const onColor2="rgba(255, 0, 0, 0.50)";
-const onColor3="rgba(255, 0, 0, 0.75)";
 const onColorFull="rgb(255, 0, 0)";
-const offColor3=onColor3
-const offColor2=onColor2;
-const offColor1=onColor1;
 const offColorFull="rgb(0, 0, 0)";
 
 var timer = setInterval(() => {
@@ -100,74 +26,6 @@ var timer = setInterval(() => {
 	else turnOff(r,b);
 }, 20);
 
-/*b=0;
-r=0;
-turnOn(r, b);
-var timer = setInterval(() => {
-	turnOff(r, b);
-	b++;
-	if (b==numberOfBits) {
-		b=0;
-		r++;
-	}
-	
-	if (r==numberOfRegisters) {
-		clearInterval(timer);
-		return;
-	}
-	
-	turnOn(r, b);
-}, 250);
-*/
-
-/*
-for (let j=0; j<256; j++) {
-	if (j>0 && j%16==0) {
-		r++;
-		b=0;
-	}
-	
-	turnOn(r, b);
-	setTimeout(() => {
-		turnOff(r, b);
-		b++;
-	}, 200);
-}
-*/
-
-/*function turnOn(register, bit) {
-	turn(register, bit, canvasColor);
-	turn (register,bit, onColor1);
-	setTimeout(() => {
-		turn(register,bit, canvasColor);
-		turn (register,bit, onColor2);
-		setTimeout(() => {
-			turn(register, bit, canvasColor);
-			turn(register,bit, onColor3);
-			setTimeout(() => {
-				turn(register, bit,canvasColor);
-				turn(register, bit, onColorFull);
-			},20);
-		},20);
-	},20);
-}
-
-function turnOff(register, bit) {
-	turn(register, bit, canvasColor);
-	turn (register,bit, offColor3);
-	setTimeout(() => {
-		turn(register,bit, canvasColor);
-		turn (register,bit, offColor2);
-		setTimeout(() => {
-			turn(register, bit, canvasColor);
-			turn(register,bit, offColor1);
-			setTimeout(() => {
-				turn(register, bit,canvasColor);
-				turn(register, bit, offColorFull);
-			},20);
-		},20);
-	},20);
-}*/
 function turnOn(register, bit) {
 	turn(register, bit, onColorFull);
 }
